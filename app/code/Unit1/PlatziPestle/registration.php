8a1761eb429db7fe1b3f19f0c6e69e89c1d6c712
yaguarnecro <yaguarnecro@gmail.com>
2021-02-10 20:47:18 +0000

    segundo comit, error namespace modules xml

---
+++ app/code/Unit1/PlatziPestle/registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Unit1_PlatziPestle',
+        __DIR__
+    );
